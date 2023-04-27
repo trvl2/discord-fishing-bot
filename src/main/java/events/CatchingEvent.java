8f1d0c72a25e8ca50f9387484ae20b75b2b68bde
Dmitrii Remiannikov <d.remyannikov@fxdev.ru>
2023-04-27 18:19:02 +0200

    Refactoring (Guard condition)

--- src/main/java/events/CatchingEvent.java
+++ src/main/java/events/CatchingEvent.java
@@ -4,24 +4,23 @@ import net.dv8tion.jda.api.entities.channel.ChannelType;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-import java.util.concurrent.TimeUnit;
+import java.util.Objects;
 
 public class CatchingEvent extends ListenerAdapter  {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event)
     {
-        if(event.getMessage().getContentRaw().equalsIgnoreCase("!catch")
-                && !event.isFromType(ChannelType.PRIVATE)
-                && !event.getAuthor().isBot())
+        if(event.isFromType(ChannelType.PRIVATE) || event.getAuthor().isBot())
         {
-            if (!event.isFromType(ChannelType.PRIVATE))
-            {
-                System.out.printf("[%s][] %s: %s\n", event.getGuild().getName(),
-                        event.getMember().getEffectiveName(),
-                        event.getMessage().getContentRaw());
-            }
+            return;
+        }
 
+        if(event.getMessage().getContentRaw().equalsIgnoreCase("!catch"))
+        {
+            System.out.printf("[%s][] %s: %s\n", event.getGuild().getName(),
+                    Objects.requireNonNull(event.getMember()).getEffectiveName(),
+                    event.getMessage().getContentRaw());
 
             event.getChannel().sendMessage("Wow! " + event.getAuthor().getName() + " now trying to catch something..").queue();
             event.getChannel().sendMessage("Great! " + event.getAuthor().getName() + " caught " + catchRandomFish()).queue();
